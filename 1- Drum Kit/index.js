ea33057e50f865b4fc3f91bf56de45ae69a34c1c
solankirishav21 <btech10645.20@bitmesra.ac.in>
2022-01-17 19:44:45 +0530

    Added Js file

--- 1- Drum Kit/index.js
+++ 1- Drum Kit/index.js
@@ -1,1 +1,19 @@
 // console.log("connected");
+const addMusic = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}" ]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}" ]`);
+
+    if(!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+const removeTransition = (e) => {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+window.addEventListener('keydown', addMusic);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
